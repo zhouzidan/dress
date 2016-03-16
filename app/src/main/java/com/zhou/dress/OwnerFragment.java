d259e21c4cd5714aa84a44c1e1642c502a9e98b2
周国宝 <zhou@zhouguobaodeiMac-5.local>
2016-03-16 20:42:04 +0800

    account some code

--- app/src/main/java/com/zhou/dress/OwnerFragment.java
+++ app/src/main/java/com/zhou/dress/OwnerFragment.java
@@ -1,5 +1,6 @@
 package com.zhou.dress;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,10 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.zhou.dress.account.LoginActivity;
+
 /**
  * Created by zhou on 16-3-8.
  */
-public class OwnerFragment extends Fragment {
+public class OwnerFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,5 +25,27 @@ public class OwnerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_owner,null);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initView();
+    }
+
+    private void initView(){
+        getView().findViewById(R.id.tv_login).setOnClickListener(this);
+        getView().findViewById(R.id.btn_registe).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_login:
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+                break;
+            case R.id.btn_registe:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/zhou/dress/account/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.zhou.dress.account;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.zhou.dress.R;
+
+/**
+ * Created by zhou on 16/3/12.
+ */
+public class LoginActivity extends Activity implements View.OnClickListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_login);
+        findViewById(R.id.layout_qq).setOnClickListener(this);
+        findViewById(R.id.layout_sina).setOnClickListener(this);
+        findViewById(R.id.layout_weixin).setOnClickListener(this);
+        findViewById(R.id.tv_foeget_pwd).setOnClickListener(this);
+        findViewById(R.id.img_back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.layout_qq:
+                break;
+            case R.id.layout_sina:
+                break;
+            case R.id.layout_weixin:
+                break;
+            case R.id.tv_foeget_pwd:
+                startActivity(new Intent(this,ForgetPwdActivity.class));
+                break;
+            case R.id.img_back:
+                finish();
+                break;
+        }
+    }
+}
